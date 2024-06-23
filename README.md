baf38ab43e04f229adaca98c68b86c2fc9c1cd1e
Muhammad Shabri rabbani <shabri@mhs.unsyiah.ac.id>
2024-06-23 15:29:05 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sistem Akademik Mahasiswa
+Sebuah program sederhana untuk input dan menghapus data mahasiswa. Menggunakan MySQL sebagai database, dan menggunakan bahasa pemrograman PHP.
+
+## Fitur
+- Input data mahasiswa
+- List data mahasiswa
+- Hapus data mahasiswa
+
+## Cara Penggunaan
+1. jalankan xampp untuk mengaktifkan apache dan mysql
+2. jalankan php -S 127.0.0.1:8000 di terminal
+```bash
+php -S 127.0.0.1:8000
+```
+3. Buka browser dan ketikkan alamat
+4. akses halaman https://127.0.0.1:8000/tampil.php
---
+++ db_akademik.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 23, 2024 at 10:28 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `umur` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `alamat`, `umur`) VALUES
+(6, 'joko', 'jakarta', '20');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
